36d2bad4d277f115935d5ba294a7a3840c1e8c2c
Nick Soper <nsoper@scottlogic.com>
2014-10-09 10:57:00 +0100

    Added projects: client app and model

--- CarTraderApp/CarTraderService/Controllers/AdvertController.cs
+++ CarTraderApp/CarTraderService/Controllers/AdvertController.cs
@@ -1,4 +1,4 @@
-﻿using CarTraderService.Models;
+﻿using CarTraderModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ CarTraderApp/CarTraderWebClient/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarTraderWebClient.Controllers
+{
+    /// <summary>
+    /// Responsible for serving up the single page for the application
+    /// </summary>
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return this.View("Index");
+        }
+
+    }
+}
