8b21cdc231ef8bd6df4309823cc9635d9d4738cb
Nick Soper <nsoper@scottlogic.com>
2014-10-09 16:17:04 +0100

    Removed console.log

--- CarTraderApp/CarTraderWebClient/app/controllers/newAdvert.js
+++ CarTraderApp/CarTraderWebClient/app/controllers/newAdvert.js
@@ -4,8 +4,6 @@
 
         $scope.createAd = function (ad) {
 
-            console.log(ad);
-
             $http.post('http://localhost:51550/api/advert', ad).then(function () {
                 $location.path('/ads');
             });
