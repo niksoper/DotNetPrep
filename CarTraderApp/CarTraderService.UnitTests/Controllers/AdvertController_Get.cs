bbbb47081124a4fa2a07dad5153aea9d315295ac
Nick Soper <nsoper@scottlogic.com>
2014-10-13 12:14:14 +0100

    Testing AdvertController.Get(int id)

--- CarTraderApp/CarTraderService.UnitTests/Controllers/AdvertController_Get.cs
+++ CarTraderApp/CarTraderService.UnitTests/Controllers/AdvertController_Get.cs
@@ -5,11 +5,15 @@ using Moq;
 using Ploeh.AutoFixture;
 using Ploeh.AutoFixture.AutoMoq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using Xunit;
 using Xunit.Extensions;
 
@@ -51,6 +55,118 @@ namespace CarTraderService.UnitTests.Controllers
                 testParameters.ForEach(paramList => sequence.Returns((IEnumerable<Advert>)paramList[0]));
 
                 return testParameters;
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(GetWithExistingIdParams))]
+        public void GetWithId_ReturnsExpectedAdvert(int id, Advert expectedAd, AdvertController sut)
+        {
+            // Act
+            var response = sut.Get(id);
+
+            // Assert
+            Advert actualAd = null;
+            response.TryGetContentValue<Advert>(out actualAd);
+
+            Assert.Same(expectedAd, actualAd);
+        }
+
+        [Theory]
+        [ClassData(typeof(GetWithExistingIdParams))]
+        public void GetWithId_ReturnsSuccessStatusCode(int id, Advert expectedAd, AdvertController sut)
+        {
+            // Act
+            var response = sut.Get(id);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory]
+        [ClassData(typeof(GetWithNonExistingIdParams))]
+        public void GetWithId_ReturnsNullContent(int id, Advert expectedAd, AdvertController sut)
+        {
+            // Act
+            var response = sut.Get(id);
+
+            // Assert
+            Advert actualAd = null;
+            response.TryGetContentValue<Advert>(out actualAd);
+
+            Assert.Null(actualAd);
+        }
+
+        [Theory]
+        [ClassData(typeof(GetWithNonExistingIdParams))]
+        public void GetWithId_ReturnsNotFoundStatusCode(int id, Advert expectedAd, AdvertController sut)
+        {
+            // Act
+            var response = sut.Get(id);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        /// <summary>
+        /// Sets up the parameters for the data-driven GetWithId_ReturnsExpectedAdvert test
+        /// </summary>
+        public abstract class GetWithIdParams : IEnumerable<object[]>
+        {
+            protected IFixture fixture;
+            private object[] testParameters;
+
+            /// <summary>
+            /// Initialises a new instance of the <see cref="GetWithIdParams"/> class
+            /// </summary>
+            public GetWithIdParams()
+            {
+                this.fixture = new Fixture().Customize(new Helpers.ApiControllerConventions());
+                var adRepoMock = fixture.Freeze<Mock<IAdvertRepository>>();
+
+                int id = 1;
+                Advert expectedAd = this.ExpectedAdvert();
+
+                // I've run into issues using AutoFixture to create this object reverted to manually creating it for now...
+                var sut = new AdvertController(adRepoMock.Object) 
+                { 
+                    Request = new HttpRequestMessage(), 
+                    Configuration = new HttpConfiguration() 
+                };
+
+                this.testParameters = new object[] { id, expectedAd, sut };
+
+                adRepoMock.Setup(x => x.GetAdvert((int)this.testParameters[0]))
+                    .Returns((Advert)this.testParameters[1]);
+            }
+
+            protected abstract Advert ExpectedAdvert();
+
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                return new List<object[]> { this.testParameters }.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
+        }
+
+        public class GetWithExistingIdParams : AdvertController_Get.GetWithIdParams
+        {
+            protected override Advert ExpectedAdvert()
+            {
+                var expectedAd = this.fixture.Create<Advert>();
+                return expectedAd;
+            }
+        }
+
+        public class GetWithNonExistingIdParams : AdvertController_Get.GetWithIdParams
+        {
+            protected override Advert ExpectedAdvert()
+            {
+                return null;
             }
         }
     }
